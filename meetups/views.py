13f587ab7da6f25db3ea1f612a7cd0d8f3d560fb
masande-99 <gontyelenimasande@gmail.com>
2021-07-06 16:01:35 +0200

    Added meetup-detail.html

--- meetups/views.py
+++ meetups/views.py
@@ -7,9 +7,20 @@ from django.http import HttpResponse
 def index(request):
     meetups = [
         {'title': 'A first meetup', 'location': 'New york', 'slug': 'a-first-meetup'},
-        {'title': 'A first meetup', 'location': 'Paris', 'slug': 'a-second-meetup'}
+        {'title': 'A second meetup', 'location': 'Paris', 'slug': 'a-second-meetup'}
     ]
     return render(request, 'meetups/index.html', {
         'show_meetups': True,
         'meetups': meetups
+    })
+
+
+def meetup_details(request, meetup_slug):
+    selected_meetup = {
+        'title': 'A first meetup',
+        'description': 'This is the first meetup'
+    }
+    return render(request, 'meetups/meetup-detail.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
     })
